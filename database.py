da6222adc49eb9a29a7b8b35a3b03df98d0881bc
Vladyslav Shepilov <shepsterya@gmail.com>
2024-07-20 22:42:08 +0300

    configured async database

---
+++ database.py
@@ -0,0 +1,19 @@
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+from settings import settings
+
+engine = create_async_engine(
+    settings.database_url,
+    echo=True,
+    connect_args={"check_same_thread": False} if "sqlite" in settings.database_url else {}
+)
+
+async_session = sessionmaker(
+    bind=engine,
+    class_=AsyncSession,
+    expire_on_commit=False
+)
+
+Base = declarative_base()
